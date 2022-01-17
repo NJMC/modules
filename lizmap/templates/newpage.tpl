864204f475b10d4fbb272d1f379f88567047e0e4
root <root@map.energydemandanalysis.com>
2022-01-17 10:43:54 +0000

    initial commit

---
+++ lizmap/templates/newpage.tpl
@@ -0,0 +1,13 @@
+<div id="header">
+  <div id="title"></div>
+  <div id="abstract"></div>
+</div>
+
+<div> WOQO QPPQ JDX XXZZZ 8888 </div>
+
+<div id="content">
+  {jmessage} 
+  {$center}
+</div>
+
+
---
+++ view/controllers/test.classic.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Displays a list of project for a given repository.
+ *
+ * @author    3liz
+ * @copyright 2012 3liz
+ *
+ * @see      http://3liz.com
+ *
+ * @license    Mozilla Public License : http://www.mozilla.org/MPL/
+ */
+class defaultCtrl extends jController
+{
+
+	public function test()
+    {
+<p> TOOTHFBDJSJSDNAHF </p>
+
+    }
+
+
+
+
+}
